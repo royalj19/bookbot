a7e0847a3a90de9ca1c65fc1e3e1a324a6fe13ad
royalj19 <royalj19@ymail.com>
2024-04-30 16:09:13 -0600

    define count word function

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ def get_book_text(path):
     with open("books/frankenstein.txt") as f:
         return f.read()
 
+def count_words(something):
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    words = text.split()
+    word_count = 0
+    for word in words:
+        word_count += 1
+    return word_count
+    
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    #print(text)
+    word_count = count_words(book_path)
+    print(word_count)
 
 main()
