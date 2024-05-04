afcc8c76b620d78a87888e0b931cc38014c4b80c
royalj19 <royalj19@ymail.com>
2024-05-04 15:55:38 -0600

    Added to function count letters to filter out non alphabet words
    Added sort_on function to return the number
    of letters counted in the list
    
    Print statements to display characters in
    alphabet ordered by number of times appeared

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 def get_book_text(path):
-    with open("books/frankenstein.txt") as f:
+    with open(path) as f:
         return f.read()
 
-def count_words(something):
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+def get_count_words(text):
     words = text.split()
-    word_count = 0
-    for word in words:
-        word_count += 1
-    return word_count
-    
+    return len(words)
+
+def get_count_letters(text):
+    lowercase = text.lower()
+    letter_dict = {}
+    for char in lowercase:
+        if  char.isalpha():
+            if char in letter_dict:
+                letter_dict[char] += 1
+            else:
+                letter_dict[char] = 1
+    letter_list = [{"letter": letter, "counted": counted} for letter, counted in letter_dict.items()]
+    return letter_list
+
+def sort_on(item):
+    return item["counted"]
+
+
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    #print(text)
-    word_count = count_words(book_path)
-    print(word_count)
+    words = get_count_words(text)
+    letters = get_count_letters(text)
+    letters.sort(key=sort_on, reverse=True)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words} words found in the document")
+    print()
+    for item in letters:
+        print(f"The '{item['letter']}' character was found {item['counted']} times")
+    print("--- End report ---")
+
+    
 
 main()
